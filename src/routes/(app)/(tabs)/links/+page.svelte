6fd76ac674b0bf0ec6a427161b21770659604938
Stefan Holmgren <stehol@gmail.com>
2024-04-15 00:20:12 +0200

    Refactor drag and drop functionality for reordering links in +page.svelte

--- src/routes/(app)/(tabs)/links/+page.svelte
+++ src/routes/(app)/(tabs)/links/+page.svelte
@@ -55,8 +55,7 @@
 		const index1 = links.indexOf(link1);
 		const index2 = links.indexOf(link2);
 		if (index1 === -1 || index2 === -1) return;
-		links[index1] = link2;
-		links[index2] = link1;
+		[links[index1], links[index2]] = [links[index2], links[index1]];
 		links = [...links];
 	}
 
