cf6378aba40d7d04abb94cee91f8f33c387ae7f7
Stefan Holmgren (uub743) <stefan.holmgren@teliacompany.com>
2025-02-17 23:08:17 +0100

    refactor LinkForm component to enhance platform selection and URL validation

--- src/components/Home/components/LinkForm/LinkForm.tsx
+++ src/components/Home/components/LinkForm/LinkForm.tsx
@@ -1,27 +1,41 @@
-import { FormEventHandler, MouseEventHandler, useEffect, useRef, useState } from "react";
+import { ChangeEventHandler, FormEventHandler, MouseEventHandler, useEffect, useRef, useState } from "react";
 import styles from "./LinkForm.module.css";
 import { User } from "@/components/AuthContext/AuthContext";
 import { useQuery } from "@tanstack/react-query";
 import { getUserLinks, UserLink } from "./utils/userLinks.utils";
 
+const platforms = ["github", "youtube"] as const;
+
+type Platform = (typeof platforms)[number];
+
 type LinkType = {
-  value: string;
+  value: Platform;
   label: string;
+  urlPattern: string;
+  exampleUrl: string;
 };
 
 const linkTypes: LinkType[] = [
   {
-    value: "youtube",
-    label: "YouTube",
-  },
-  {
     value: "github",
     label: "GitHub",
+    urlPattern: "https://github\\.com/.+",
+    exampleUrl: "https://github.com/<username>",
+  },
+  {
+    value: "youtube",
+    label: "YouTube",
+    urlPattern: "https://www.youtube.com/@.+",
+    exampleUrl: "https://www.youtube.com/@<username>",
   },
 ];
 
 type LinkFormProps = {
   user: User;
+};
+
+const isPlatform = (value: string): value is Platform => {
+  return platforms.includes(value as Platform);
 };
 
 export const LinkForm = ({ user }: LinkFormProps) => {
@@ -44,15 +58,27 @@ export const LinkForm = ({ user }: LinkFormProps) => {
     e.preventDefault();
   };
 
-  const onAddNewLink: MouseEventHandler<HTMLButtonElement> = (e) => {
+  const onAddNewLink: MouseEventHandler<HTMLButtonElement> = () => {
     if (!formRef.current?.reportValidity()) {
       return;
     }
 
-    setCurrentUserLinks((prev) => [...(prev ? prev : []), { url: "", platform: "youtube" }]);
+    setCurrentUserLinks((prev) => [...(prev ? prev : []), { url: "", platform: linkTypes[0].value }]);
 
     lastSelectRef.current?.focus();
   };
+
+  const onPlatformChanged =
+    (index: number): ChangeEventHandler<HTMLSelectElement> =>
+    (e) => {
+      setCurrentUserLinks((prev) => {
+        if (isPlatform(e.target.value) && prev?.[index]) {
+          prev[index].platform = e.target.value;
+          return [...prev];
+        }
+        return prev;
+      });
+    };
 
   return (
     <form className={styles["link-form"]} onSubmit={onSubmit} ref={formRef}>
@@ -63,18 +89,29 @@ export const LinkForm = ({ user }: LinkFormProps) => {
         <div>Loading...</div>
       ) : (
         <ul>
-          {currentUserLinks.map((userLink, i) => (
-            <li key={userLink.url}>
-              <select aria-label="Platform" required ref={i === currentUserLinks.length - 1 ? lastSelectRef : undefined}>
-                {linkTypes.map(({ value, label }) => (
-                  <option value={value} selected={userLink.platform === value}>
-                    {label}
-                  </option>
-                ))}
-              </select>
-              <input type="url" defaultValue={userLink.url} required></input>
-            </li>
-          ))}
+          {currentUserLinks.map((userLink, i) => {
+            const currentLinkType = linkTypes.find((linkType) => linkType.value === userLink.platform);
+
+            return (
+              <li key={userLink.url}>
+                <select aria-label="Platform" required ref={i === currentUserLinks.length - 1 ? lastSelectRef : undefined} onChange={onPlatformChanged(i)}>
+                  {linkTypes.map(({ value, label }) => (
+                    <option value={value} selected={userLink.platform === value}>
+                      {label}
+                    </option>
+                  ))}
+                </select>
+                <input
+                  type="url"
+                  defaultValue={userLink.url}
+                  required
+                  placeholder={currentLinkType?.exampleUrl ?? ""}
+                  pattern={currentLinkType?.urlPattern}
+                  title={"Please check the URL"}
+                ></input>
+              </li>
+            );
+          })}
         </ul>
       )}
       <button type="submit">Save</button>
