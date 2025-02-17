a2d0eb3e35e69935922ba132ccc1cc5baf38baa4
Stefan Holmgren (uub743) <stefan.holmgren@teliacompany.com>
2025-02-17 23:08:12 +0100

    remove unused useGetUserLinks hook from LinkForm component

--- src/components/Home/components/LinkForm/hooks/useGetUserLinks.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "@/components/AuthContext/AuthContext";
-
-type UseGetUserLinksProps = {
-  user: User;
-};
-
-export const useGetUserLinks = ({ user }: UseGetUserLinksProps) => {};
