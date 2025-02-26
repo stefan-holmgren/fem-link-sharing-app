8662f4a776044233b296e8471f7980e584bde93b
Stefan Holmgren (uub743) <stefan.holmgren@teliacompany.com>
2025-02-26 21:26:13 +0100

    feat: enhance Snackbar component with drag-to-dismiss functionality using motion

--- src/components/SnackbarContext/components/Snackbar/Snackbar.tsx
+++ src/components/SnackbarContext/components/Snackbar/Snackbar.tsx
@@ -1,16 +1,39 @@
 import { HTMLAttributes, ReactNode } from "react";
 import styles from "./Snackbar.module.css";
-type SnackbarProps = Omit<HTMLAttributes<HTMLDivElement>, "onClick"> & {
+import { useAnimation, motion, HTMLMotionProps, PanInfo } from "motion/react";
+
+type SnackbarProps = Omit<HTMLAttributes<HTMLMotionProps<"div">>, "onClick"> & {
   message: string;
   icon?: ReactNode;
   onClose?: () => void;
 };
 
-export const Snackbar = ({ className = "", icon, message, onClose, ...rest }: SnackbarProps) => {
+export const Snackbar = ({ className = "", icon, message, onClose }: SnackbarProps) => {
+  const controls = useAnimation();
+
+  const handleDragEnd = (info: PanInfo) => {
+    if (Math.abs(info.offset.x) > 100) {
+      controls.start({
+        x: info.offset.x > 0 ? "100%" : "-100%",
+        transition: {
+          duration: 0.2,
+        },
+      });
+      onClose?.();
+    }
+  };
+
   return (
-    <div className={`${styles.snackbar} ${className}`} onClick={() => onClose?.()} {...rest}>
+    <motion.div
+      drag="x"
+      dragConstraints={{ left: 0, right: 0 }}
+      animate={controls}
+      onDragEnd={(_, info) => handleDragEnd(info)}
+      className={`${styles.snackbar} ${className}`}
+      onClick={() => onClose?.()}
+    >
       {icon}
       <p>{message}</p>
-    </div>
+    </motion.div>
   );
 };
